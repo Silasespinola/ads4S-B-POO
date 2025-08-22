8b7ab9287dca0410c103e69008bcb64ce0099788
Silasespinola <silasespinola16@gmail.com>
2025-08-21 22:18:12 -0300

    Primeiro commit

---
+++ projeto-silas/src/main/java/com/bn/projeto_silas/ProjetoSilasApplication.java
@@ -0,0 +1,13 @@
+package com.bn.projeto_silas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSilasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSilasApplication.class, args);
+	}
+
+}
---
+++ projeto-silas/src/test/java/com/bn/projeto_silas/ProjetoSilasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bn.projeto_silas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSilasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
