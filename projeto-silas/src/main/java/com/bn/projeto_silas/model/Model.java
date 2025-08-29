7aca8d4603be25c4e3a0fe8b4ac20cb63a8ade0d
silas espinola <silasespinola16@gmail.com>
2025-08-28 21:54:03 -0300

    model feita

--- projeto-silas/src/main/java/com/bn/projeto_silas/model/Model.java
+++ projeto-silas/src/main/java/com/bn/projeto_silas/model/Model.java
@@ -1,4 +1,36 @@
 package com.bn.projeto_silas.model;
 
 public class Model {
-}
+
+    private int id;
+    private String nome;
+    private int idade;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+};
+
+
+
+
